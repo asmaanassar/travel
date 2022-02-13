cf41a0001fd11a4527bc0add16b65e0debb10df0
asmaanassar <asmaanassar89@gmail.com>
2022-02-13 20:58:14 +0200

    day16

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Header(props) {
+    return (
+
+        <>
+            <h1 className="App-header" >Hello  {props.fName}   {props.lName} </h1>
+        </>
+
+    );
+}
+
+export default Header;
---
+++ src/components/Tours/Tours.js
@@ -0,0 +1,22 @@
+
+
+function Tours(props) {
+
+    return (
+        <>
+            {props.data.map(item => {
+                return (
+                    <div>
+                        <img src={item.image} alt={item.name} />
+                        <h3>Name : {item.name}</h3>
+                        <p>Price : {item.price}</p>
+                        <p>Info:{item.info}</p>
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default Tours;
+
